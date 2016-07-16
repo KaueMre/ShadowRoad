81d27cadee4b71bb0bca84798f3db125dc822e8a
Tony <tonysaltodev@gmail.com>
2016-07-16 00:32:30 -0300

    comitando primeiro esboco

--- Assets/_ShadowRoad/Scripts/PlayerController.cs
+++ Assets/_ShadowRoad/Scripts/PlayerController.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
+    private Rigidbody2D playerRB;
+    private float horinzontalInput;
+    private float verticalInput;
+
+    [SerializeField] private float speed;
+
+    
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        CheckInput();
+        CheckMovement();
+    }
+
+    private void CheckInput()
+    {
+        horinzontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void CheckMovement()
+    {
+        Vector2 movement = new Vector2(horinzontalInput, verticalInput);
+        playerRB.velocity = movement * speed * Time.deltaTime;
+    }
+
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
